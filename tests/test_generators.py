bc6ae268f3867ba0836b8894830d4708c3a8e90b
Till Hoffmann <tillahoffmann@gmail.com>
2022-04-21 18:36:41 +0100

    Add seed testing.

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from simulation_based_graph_inference.graph import Graph
 from simulation_based_graph_inference import generators
@@ -16,3 +17,12 @@ def test_generate_poisson_random_attachment(graph: Graph):
     rate = th.distributions.Gamma(4, 1).sample()
     generators.generate_poisson_random_attachment(100, rate, graph)
     assert graph.get_num_nodes() == 100
+
+
+def test_seed():
+    edge_indices = []
+    for _ in range(2):
+        generators.set_seed(42)
+        graph = generators.generate_poisson_random_attachment(10, 3)
+        edge_indices.append(graph.to_edge_index())
+    np.testing.assert_array_equal(*edge_indices)
