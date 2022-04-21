aac44a6b3d2fafaa86ffaa38c73f5e96b5b00241
Till Hoffmann <tillahoffmann@gmail.com>
2022-04-21 13:39:16 +0100

    Always convert to edge index to find any bugs in generators.

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,8 +1,18 @@
+import pytest
 from simulation_based_graph_inference.graph import Graph
 from simulation_based_graph_inference import generators
+import torch as th
 
 
-def test_generate_poisson_random_attachment():
-    graph: Graph = generators.generate_poisson_random_attachment(100, 4.5)
+@pytest.fixture
+def graph():
+    graph = Graph()
+    yield graph
+    # Always convert to edge index to find any bugs in generators.
+    graph.to_edge_index()
+
+
+def test_generate_poisson_random_attachment(graph: Graph):
+    rate = th.distributions.Gamma(4, 1).sample()
+    generators.generate_poisson_random_attachment(100, rate, graph)
     assert graph.get_num_nodes() == 100
-    assert 4 < graph.get_num_edges() / 100 < 5
