29c6a6358608fab8ee86f4d5d7a1c591eabd6e3e
Till Hoffmann <tillahoffmann@gmail.com>
2022-05-23 15:52:11 -0400

    Use a more diverse set of architectures.

--- beaver.py
+++ beaver.py
@@ -3,27 +3,54 @@ import itertools as it
 import os
 from simulation_based_graph_inference.scripts.util import GENERATORS
 
+# Prevent each process from parallelizing which can lead to competition across processes.
 bb.Subprocess.ENV.update({
     "NUMEXPR_NUM_THREADS": 1,
     "OPENBLAS_NUM_THREADS": 1,
     "OMP_NUM_THREADS": 1,
 })
 
+# Load basic configuration from the environment.
 CONFIG = {
     "MAX_DEPTH": (int, 4),
     "NUM_SEEDS": (int, 10),
 }
 CONFIG = {key: type(os.environ.get(key, default)) for key, (type, default) in CONFIG.items()}
 
+# Set up different architecture specifications that we would like to compare.
 DEPTHS = range(CONFIG["MAX_DEPTH"] + 1)
 SEEDS = range(CONFIG["NUM_SEEDS"])
-for generator, depth, seed in it.product(GENERATORS, DEPTHS, SEEDS):
-    with bb.group_artifacts("workspace", "sinm2022", generator, f"depth_{depth}"):
-        dense = "8,8"
-        if depth == 0:
-            conv = "none"
-        else:
-            conv = "_".join(["simple"] + ["norm"] * (depth - 1))
+
+SPECIFICATIONS = {}
+for depth in DEPTHS:
+    # Create simple convolutional isomorphism layers with normalization for all but the first layer.
+    conv = "_".join(["simple"] + ["norm"] * (depth - 1)) if depth else "none"
+    SPECIFICATIONS[("simple-narrow", f"depth-{depth}")] = {
+        "dense": "8,8",
+        "conv": conv,
+    }
+    SPECIFICATIONS[("simple-deep", f"depth-{depth}")] = {
+        "dense": "8,8,8,8",
+        "conv": conv,
+    }
+    SPECIFICATIONS[("simple-wide", f"depth-{depth}")] = {
+        "dense": "64,64",
+        "conv": conv,
+    }
+
+    # Create convolutional isomorphism layers with two-layer dense networks after each layer.
+    SPECIFICATIONS[("gin-narrow", f"depth-{depth}")] = {
+        "dense": "8,8",
+        "conv": "_".join(["8,8"] * depth) if depth else "none",
+    }
+    SPECIFICATIONS[("gin-medium", f"depth-{depth}")] = {
+        "dense": "16,16",
+        "conv": "_".join(["16,16"] * depth) if depth else "none",
+    }
+
+# Generate targets for the different configurations.
+for generator, seed, (key, specification) in it.product(GENERATORS, SEEDS, SPECIFICATIONS.items()):
+    with bb.group_artifacts("workspace", "sinm2022", generator, *key):
         args = ["$!", "-m", "simulation_based_graph_inference.scripts.sinm2022", "--test=$@",
-                f"--seed={seed}", generator, conv, dense]
-        bb.Subprocess(f"seed_{seed}.pkl", None, args)
+                f"--seed={seed}", generator, specification["conv"], specification["dense"]]
+        bb.Subprocess(f"seed-{seed}.pkl", None, args)
