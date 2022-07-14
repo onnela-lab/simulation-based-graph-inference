a0cc137a841e6bc05865cd49f22d46128e66f150
Till Hoffmann <tillahoffmann@gmail.com>
2022-07-14 16:37:18 -0400

    Add fixture for temporary working directory.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,16 @@
+import os
+import pytest
+import tempfile
 import torch as th
 
 
 th.set_default_dtype(th.float64)
+
+
+@pytest.fixture
+def tmpwd():
+    with tempfile.TemporaryDirectory() as tmp:
+        cwd = os.getcwd()
+        os.chdir(tmp)
+        yield tmp
+        os.chdir(cwd)
