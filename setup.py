098acb06033475a3a4861805df5c0f01ad0bc801
Till Hoffmann <tillahoffmann@gmail.com>
2022-07-15 11:18:13 -0400

    Update requirements.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     version="0.1.0",
     install_requires=[
         "cython",
-        "doit-interface>=0.1.5",
+        "doit-interface>=0.1.6",
         "matplotlib",
         "networkx",
         "numpy",
