22eb636e3b601f4f8fe663892d4e0e88411c67b4
Till Hoffmann <tillahoffmann@gmail.com>
2022-07-19 13:29:00 -0400

    Ensure torch `Transform` is pickleable.

--- simulation_based_graph_inference/__init__.py
+++ simulation_based_graph_inference/__init__.py
@@ -1,1 +1,12 @@
-pass
+import torch as th
+
+
+# Hack: do not save `_inv` which is a `weakref` that cannot be pickled (see
+# https://github.com/pytorch/pytorch/pull/81707 for context and a fix).
+def _Transform_getstate(self):
+    state = self.__dict__.copy()
+    state["_inv"] = None
+    return state
+
+
+th.distributions.Transform.__getstate__ = _Transform_getstate
