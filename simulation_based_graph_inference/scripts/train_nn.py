04d24fd65eb1431a00bc4f59905c652163d55c6a
Till Hoffmann <tillahoffmann@gmail.com>
2022-08-26 18:45:13 -0400

    Pin training to one CPU.

--- simulation_based_graph_inference/scripts/train_nn.py
+++ simulation_based_graph_inference/scripts/train_nn.py
@@ -83,6 +83,8 @@ def run_epoch(model: models.Model, loader: DataLoader, epsilon: float,
 
 
 def __main__(args: typing.Optional[list[str]] = None) -> None:
+    th.set_num_threads(1)
+    th.set_num_interop_threads(1)
     parser = get_parser(100)
     parser.add_argument("--batch_size", "-b", help="batch size for each optimization step",
                         type=int, default=32)
