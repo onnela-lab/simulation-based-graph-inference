77ca0dcf9f70ddac5b995e694c5a6305ce4ae85e
Till Hoffmann <tillahoffmann@gmail.com>
2022-08-31 20:36:00 -0400

    Install `fasttr` for tree inference.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     version="0.1.0",
     install_requires=[
         "doit-interface>=0.1.6",
+        "fasttr @ git+https://github.com/gstonge/fasttr",
         "matplotlib",
         "networkx",
         "numpy",
